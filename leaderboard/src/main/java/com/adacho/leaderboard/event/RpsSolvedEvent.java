49b97840bd4ebeb6d1d5bde5a40eadaf87498a65
jonghyeok <jonghyeok.bae@gmail.com>
2023-11-21 13:09:56 +0900

    second leaderboard commit

---
+++ leaderboard/src/main/java/com/adacho/leaderboard/event/RpsSolvedEvent.java
@@ -0,0 +1,30 @@
+package com.adacho.leaderboard.event;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class RpsSolvedEvent implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7055561106743916191L;
+	private final Long rpsChallengeId;
+	private final Long userId;
+	private final String outcome;
+	
+	// JSON 역직렬화에 필요
+	RpsSolvedEvent() {
+		this.rpsChallengeId = 0L;
+		this.userId = 0L;
+		this.outcome = null;
+	}
+}
